dc7d1afe5f2a935adccca82dbab28f5a0dff6044
Zoob-air <2211510629@student.budiluhur.ac.id>
2024-08-16 16:17:21 +0700

    data baru

--- full_backup.sh
+++ full_backup.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-# Set the base backup path
-DB_NAME="infamdmdev"
+# INPUT the VARIABEL
+DB_NAME="zuber"
+TYPESCRIPT="fullbackup"
 
-BASE_BACKUP_PATH="/backup/FULLBACKUP/$DB_NAME"
+BASE_BACKUP_PATH="/home/oracle/skrip/$TYPESCRIPT/$DB_NAME"
 
 # Source the Oracle environment
-. /home/oracle/.profilemdmdev
+. /home/oracle/db19c.env
 
 # Get the current date
 BCK=$(date "+%Y%m%d")
 
-#old backup
+#set old backup
 BCK_OLD=$(date -d "$BCK -1 month" +"%Y%m%d")
 
-# Create the backup directory
-mkdir -p "$BASE_BACKUP_PATH/backup_full_$BCK"
-
 # Set the RMAN log path
-RMAN_LOG_PATH="/home/backup-data/rmanbackup/$DB_NAME/fullbackuplogs"
+RMAN_LOG_PATH="$BASE_BACKUP_PATH/fullbackup_log"
 
-# Run RMAN backup script
+# Create the backup directory
+mkdir -p "$BASE_BACKUP_PATH/${TYPESCRIPT}_${BCK}"
+mkdir -p "$RMAN_LOG_PATH"
+
+
+ Run RMAN backup script
 rman target / LOG="$RMAN_LOG_PATH/backup_full_$DB_NAME_$BCK.log" <<EOF
 run
 {
@@ -33,9 +36,9 @@ run
   allocate channel ch07 type disk maxpiecesize=1024M;
   allocate channel ch08 type disk maxpiecesize=1024M;
   delete noprompt backup;
-  backup as compressed backupset database format '$BASE_BACKUP_PATH/backup_full_$BCK/df_full_%T_d%d_p%p_u%u_c%c.bak';
-  backup as compressed backupset current controlfile format '$BASE_BACKUP_PATH/backup_full_$BCK/cf_full_%T_d%d_p%p_u%u_c%c.ctl';
-  backup as compressed backupset current controlfile for standby format '$BASE_BACKUP_PATH/backup_full_$BCK/standby_cf_full_%T_d%d_p%p_u%u_c%c.ctl';
+  backup as compressed backupset database format '$BASE_BACKUP_PATH/${TYPESCRIPT}_${BCK}/df_full_%T_d%d_p%p_u%u_c%c.bak';
+  backup as compressed backupset current controlfile format '$BASE_BACKUP_PATH/${TYPESCRIPT}_${BCK}/cf_full_%T_d%d_p%p_u%u_c%c.ctl';
+  backup as compressed backupset current controlfile for standby format '$BASE_BACKUP_PATH/${TYPESCRIPT}_${BCK}/standby_cf_full_%T_d%d_p%p_u%u_c%c.ctl';
   release channel ch01;
   release channel ch02;
   release channel ch03;
@@ -47,10 +50,12 @@ run
 }
 EOF
 
+#DELETE OLD BACKUP
+#============================================================================================
 # Copy the backup directory to the destination
-cp -r "$BASE_BACKUP_PATH/backup_full_$BCK" "/home/backup-data/rmanbackup/$DB_NAME/fullbackup"
+#cp -r "$BASE_BACKUP_PATH/backup_full_$BCK" "/home/backup-data/rmanbackup/$DB_NAME/fullbackup"
 
 # Remove the backup on temporary directory after successful copy
-rm -r "$BASE_BACKUP_PATH/backup_full_$BCK"
+#rm -r "$BASE_BACKUP_PATH/backup_full_$BCK"
 #remove old backup
-rm -r "/home/backup-data/rmanbackup/$DB_NAME/fullbackup/backup_full_$BCK_OLD"
+#rm -r "/home/backup-data/rmanbackup/$DB_NAME/fullbackup/backup_full_$BCK_OLD"
